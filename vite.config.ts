ba136b51b1c8b8f787c596655a1cbe95d74550aa
mini--inim <forestaroundseoul@gmail.com>
2025-05-02 04:03:28 +0900

    viteconfig 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,36 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
+import path, { resolve } from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-3/" : "/",
   server: {
     proxy: {
       "/api": {
+        // target: 'https://jsonplaceholder.typicode.com',
         target: "https://dummyjson.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-  define: {
-    "process.env.API_BASE_URL": JSON.stringify(
-      process.env.NODE_ENV === "production" ? "https://dummyjson.com" : "/api",
-    ),
-  },
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "index.html"),
       },
+    },
+  },
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-3/" : "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@app": path.resolve(__dirname, "./src/app"),
     },
   },
 })
