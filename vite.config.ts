ca30ccca45b0206c1035816808eeb64ca3844a3d
mini--inim <forestaroundseoul@gmail.com>
2025-05-02 03:53:29 +0900

    빌드 경로

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-3/" : "/",
   server: {
     proxy: {
       "/api": {
-        // target: 'https://jsonplaceholder.typicode.com',
         target: "https://dummyjson.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  define: {
+    "process.env.API_BASE_URL": JSON.stringify(
+      process.env.NODE_ENV === "production" ? "https://dummyjson.com" : "/api",
+    ),
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
       },
     },
   },
